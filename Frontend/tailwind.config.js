0332488c6bcbdbf6c1b56227290e6cc5155cc9b1
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-10-01 12:41:18 -0300

    Setup colors and themes on tailwind config

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,7 +3,30 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  ],  
+  theme: {
+    screens:{
+      mobileS: "320px",
+      mobileM: "375px",
+      mobileL: "425px",
+      tablet: "768px",
+      laptop: "1024px",
+      laptopL: "1440px",
+      monitor: "1920px"
+    },
+    colors:{
+      "primary": "#463772",
+      "secondary": "#806FB6",
+      "green": "#50FF50",
+      "red": "#FF5050",
+      "blue": "#5050FF",
+      "yellow": "#FFFF50",
+      "white": "#FEFEFE",
+      "black": "#010101",
+    },
+    extend: {
+    },
+  },
   plugins: [],
 }
 
