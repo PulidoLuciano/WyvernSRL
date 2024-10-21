64c74af8ef7a2a6ae38a075370cd16a33416ae4b
FelipeSarco17 <sarcofelipe456@gmail.com>
2024-10-21 17:28:45 -0300

    Responsive style added. Data table and pagination finished.

--- Frontend/src/pages/ClientsModule.tsx
+++ Frontend/src/pages/ClientsModule.tsx
@@ -1,17 +1,103 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import filterIcon from "../images/filterIcon.svg"
 import Accordion from '../components/Accordion'
+import Pagination from '../components/Pagination';
 
 
 
 const ClientsModule = () => {
 
-  const [selectedAll, setSelectedAll] = useState(false);
+  const [selectedAll, setSelectedAll] = useState<boolean>(false);
+  const [dataLength, setDataLength] = useState<number>(10);
+  const [currentPage, setCurrentPage] = useState<number>(1)
+
+  const data = [
+    {
+      name: "jesus",
+      phone: "123123",
+      email: "jesus@gmail.com",
+      suscription: true,
+      pais: "argentina"
+    },
+    {
+      name: "jesus",
+      phone: "123123",
+      email: "jesus@gmail.com",
+      suscription: true,
+      pais: "argentina"
+    }, {
+      name: "jesus",
+      phone: "123123",
+      email: "jesus@gmail.com",
+      suscription: true,
+      pais: "argentina"
+    }, {
+      name: "jesus",
+      phone: "123123",
+      email: "jesus@gmail.com",
+      suscription: true,
+      pais: "argentina"
+    }, {
+      name: "jesus",
+      phone: "123123",
+      email: "jesus@gmail.com",
+      suscription: true,
+      pais: "argentina"
+    }, {
+      name: "jesus",
+      phone: "123123",
+      email: "jesus@gmail.com",
+      suscription: true,
+      pais: "argentina"
+    }, {
+      name: "jesus",
+      phone: "123123",
+      email: "jesus@gmail.com",
+      suscription: true,
+      pais: "argentina"
+    }, {
+      name: "jesus",
+      phone: "123123",
+      email: "jesus@gmail.com",
+      suscription: true,
+      pais: "argentina"
+    }, {
+      name: "jesus",
+      phone: "123123",
+      email: "jesus@gmail.com",
+      suscription: true,
+      pais: "argentina"
+    }, {
+      name: "jesus",
+      phone: "123123",
+      email: "jesus@gmail.com",
+      suscription: true,
+      pais: "argentina"
+    }, {
+      name: "jesus",
+      phone: "123123",
+      email: "jesus@gmail.com",
+      suscription: true,
+      pais: "argentina"
+    }
+  ]
+
+  //PAGINATION
+
+  const indexEnd = currentPage * dataLength;
+  const indexStart = indexEnd - dataLength;
+  const nPages = Math.ceil(data.length / dataLength);
+  const dataShown = data.slice(indexStart, indexEnd);
+  const changePage = (nextPage: number) => {
+    setCurrentPage(nextPage);
+  }
+
+
+  //********
 
   const handleCheckAll = () => {
     setSelectedAll(!selectedAll)
   }
-
 
 
   const handleCheck = () => {
@@ -19,16 +105,16 @@ const ClientsModule = () => {
   }
 
   return (
-    <main className='p-12'>
+    <main className=' w-full p-3 tablet:p-12 '>
 
-      <div className='flex items-end gap-6'>
-        <h1>Modulo Clientes</h1>
+      <div className='flex flex-col items-start gap-y-3 tablet:gap-6'>
+        <h1 className='text-4xl'>Modulo Clientes</h1>
         <p>Ver, crear, editar y eliminar clientes</p>
       </div>
 
 
       <Accordion title="Filtrar por">
-        <form action="" className='grid grid-cols-3 grid-rows-3 gap-y-12 gap-x-64' >
+        <form action="" className='grid grid-rows-7 grid-cols-1 gap-y-3 tablet:grid-cols-3 tablet:grid-rows-3 tablet:gap-x-12 tablet:gap-y-12 laptopL:gap-x-64' >
           <div className='flex flex-col'>
             <label htmlFor="nombreCliente">Nombre</label>
             <input className='bg-gray border border-gray2 text-sm rounded-lg focus:ring-blue focus:border-blue block w-full p-2.5' type="text" id='nombreCliente' placeholder='Username' />
@@ -66,7 +152,7 @@ const ClientsModule = () => {
             </select>
           </div>
 
-          <button type='submit' className='text-white bg-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 col-start-3 place-self-end'>
+          <button type='submit' className='text-white bg-primary my-3 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center justify-center tablet:me-2 tablet:col-start-3 tablet:place-self-end'>
             <img src={filterIcon} alt="filterIcon" />
             Filtrar
           </button>
@@ -74,16 +160,20 @@ const ClientsModule = () => {
         </form>
       </Accordion>
 
-      <div className='grid grid-rows-1 grid-cols-6 gap-x-2'>
-        <h2>Clientes</h2>
-        <p className='self-center'>Página 1 de 20</p>
-        <button className='bg-red font-semibold col-start-5 col-end-6 rounded flex gap-2 items-center justify-center'>
+      <div className='grid grid-rows-3 gap-y-3 tablet:gap-x-2 tablet:grid-rows-1 tablet:grid-cols-4 laptop:gap-x-2 laptopL:grid-cols-6'>
+        <div className='flex gap-2 items-end tablet:col-span-2'>
+          <h2>Clientes</h2>
+          <p>Página 1 de 20</p>
+        </div>
+
+        <button className='bg-red font-semibold text-sm rounded flex items-center justify-center p-3 tablet:col-start-3 tablet:gap-2 laptopL:col-start-5 laptopL:col-end-6'>
           <svg className="w-6 h-6 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
           </svg>
+          Eliminar Seleccionados (0)
+        </button>
 
-          Eliminar Seleccionados (0)</button>
-        <button className='bg-primary font-semibold col-start-6 col-end-7 rounded flex gap-2 items-center justify-center text-white'>
+        <button className='bg-primary font-semibold laptopL:col-start-6 laptopL:col-end-7 rounded flex gap-2 items-center justify-center text-white'>
           <svg className="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
           </svg>
@@ -91,53 +181,48 @@ const ClientsModule = () => {
         </button>
       </div>
 
+      <div className='overflow-x-auto mt-6'>
+        <table className='w-full'>
+          <thead className=''>
+            <th className='p-2 flex text-start '><input type="checkbox" onChange={handleCheckAll} className='me-2' />Nombre</th>
+            <th className='p-2 text-start '>Plataforma</th>
+            <th className='p-2 text-start '>País</th>
+            <th className='p-2 text-start '>Suscripto</th>
+            <th className='p-2 text-start '>Correo</th>
+            <th className='p-2 text-start '></th>
+          </thead>
+          <tbody className=''>
+            {dataShown.map(cliente => {
+              return (<>
+                <tr>
+                  <td className=' p-2 '><input type="checkbox" className='me-2' />{cliente.name}</td>
+                  <td className=' p-2 '>steam</td>
+                  <td className=' p-2 '>{cliente.pais}</td>
+                  <td className=' p-2 '>{cliente.suscription ? "Si" : "No"}</td>
+                  <td className=' p-2 '>{cliente.email}</td>
+                  <td className='flex justify-center items-center gap-2 p-2 '>
+                    <svg className="w-6 h-6 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                      <path stroke="currentColor" strokeWidth="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
+                      <path stroke="currentColor" strokeWidth="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
 
-      <table className='w-full'>
-        <thead className=''>
-          <th className='p-2 text-start '><input type="checkbox" onChange={handleCheckAll} className='me-2' /> Nombre</th>
-          <th className='p-2 text-start '>Plataforma</th>
-          <th className='p-2 text-start '>País</th>
-          <th className='p-2 text-start '>Suscripto</th>
-          <th className='p-2 text-start '>Correo</th>
-          <th className='p-2 text-start '></th>
-        </thead>
-        <tbody>
-          <td className=' p-2 '><input type="checkbox" className='me-2' /> Juan</td>
-          <td className=' p-2 '>steam</td>
-          <td className=' p-2 '>argentina</td>
-          <td className=' p-2 '>si</td>
-          <td className=' p-2 '>juan@gmail.com</td>
-          <td className='flex justify-center items-center gap-2 p-2 '>
-            <svg className="w-6 h-6 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" strokeWidth="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
-              <path stroke="currentColor" strokeWidth="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
+                    <svg className="w-6 h-6 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                    </svg>
 
-            <svg className="w-6 h-6 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
-            </svg>
+                  </td>
+                </tr>
+              </>)
+            })}
+          </tbody>
+        </table>
+      </div>
 
-          </td>
-        </tbody>
-      </table>
 
-      <div className='flex items-center justify-end gap-6 my-6' id='paginacionTabla'>
-        <span>
-          <svg className="w-6 h-6 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14M5 12l4-4m-4 4 4 4" />
-          </svg>
+      <div className='flex items-center justify-center laptop:justify-end gap-6 my-6' id='paginacionTabla'>
 
-        </span>
-        <a href="" className='bg-primary rounded-md p-3 text-white'>1</a>
-        <a href="">2</a>
-        <a href="">3</a>
-        <a href="">4</a>
-        <span>
-          <svg className="w-6 h-6 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
-          </svg>
+        <Pagination changePage={changePage} nPages={nPages} currentPage={currentPage} indexStart={indexStart} indexEnd={indexEnd} />
 
-        </span>
       </div>
 
     </main>
