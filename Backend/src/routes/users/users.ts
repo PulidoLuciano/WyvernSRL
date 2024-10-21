8ebab4fd86a630c0fff5b743153508da855ff27a
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-10-21 14:07:00 -0300

    Setup prisma ORM in backend project

--- Backend/src/routes/users/users.ts
+++ Backend/src/routes/users/users.ts
@@ -1,9 +1,12 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 
+const prisma = new PrismaClient()
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("User");
+router.get("/", async (req, res) => {
+    const users = await prisma.usuarios.findMany()
+    res.json(users)
 });
 
 export {router as UsersRouter};
