26257a47264d8ea596c4e5c535567fdad5f3702d
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-10-21 14:09:16 -0300

    Delete imports of not existing routers

--- Backend/src/routes/ROUTERS.ts
+++ Backend/src/routes/ROUTERS.ts
@@ -1,24 +1,9 @@
-import { ArticlesRouter } from "./articles/articles";
-import { CategoriesRouter } from "./categories/categories";
-import { CommentsRouter } from "./comments/comments";
 import { UsersRouter } from "./users/users"
 
 const ROUTERS = [
     {
         path: "/users",
         router: UsersRouter
-    },
-    {
-        path: "/articles",
-        router: ArticlesRouter
-    },
-    {
-        path: "/comments",
-        router: CommentsRouter
-    },
-    {
-        path: "/categories",
-        router: CategoriesRouter
     }
 ]
 
