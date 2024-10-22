83faa7fe0c734915ef5d5d53fd79f916556b43b6
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-10-22 09:47:43 -0300

    Add encryption to populated passwords

--- Backend/src/utils/data/USUARIOS.ts
+++ Backend/src/utils/data/USUARIOS.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client"
 import EMPLEADOS from "./EMPLEADOS"
 import ROLES from "./ROLES"
+import { hashSync } from "bcrypt"
 
 export default (prisma : PrismaClient) => ({
     dependencies: [EMPLEADOS, ROLES],
@@ -8,31 +9,31 @@ export default (prisma : PrismaClient) => ({
     data: [
         {
           "nombre": "JuanPerez1",
-          "contrasenia": "1234",
+          "contrasenia": hashSync("1234",10),
           "Empleados_id": 1,
           "Roles_id": 1
         },
         {
             "nombre": "MariaGonzalez2",
-            "contrasenia": "1234",
+            "contrasenia": hashSync("1234",10),
             "Empleados_id": 2,
             "Roles_id": 2
         },
         {
             "nombre": "CarlosDiaz3",
-            "contrasenia": "1234",
+            "contrasenia": hashSync("1234",10),
             "Empleados_id": 3,
             "Roles_id": 3
         },
         {
             "nombre": "LuciaFernandez4",
-            "contrasenia": "1234",
+            "contrasenia": hashSync("1234",10),
             "Empleados_id": 4,
             "Roles_id": 4
         },
         {
             "nombre": "JorgeRamirez5",
-            "contrasenia": "1234",
+            "contrasenia": hashSync("1234",10),
             "Empleados_id": 5,
             "Roles_id": 5
         }
