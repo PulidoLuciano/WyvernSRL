2fb92fbd40ec0d066d3e4cc8775c793406ea1a33
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-10-24 10:25:58 -0300

    Implement login endpoint

--- Backend/src/routes/users/users.ts
+++ Backend/src/routes/users/users.ts
@@ -1,26 +1,64 @@
 import { ROLE } from "../../utils/Role.ts"
 import type { WyvernRoute } from "../../types.d.ts"
-import { getAllUsers } from "./usersController"
+import { getAllUsers, login } from "./usersController"
 import ApiError from "../../utils/ApiError.ts"
 import HttpStatuses from "../../utils/HttpStatus.ts"
 
 const USERS_ROUTES : Array<WyvernRoute> = [
     {
+        //Traer todos los usuarios
         path: "/",
         method: "GET",
         authentication: false,
-        authorization: [ROLE.Admin],
+        authorization: [ROLE.Admin, ROLE.Auditor],
         middlewares: [],
         handler: getAllUsers
     },
     {
+        //Login de usuario con cookie
         path: "/login",
         method: "GET",
         authentication: false,
         authorization: [],
         middlewares: [],
+        handler: login
+    },
+    {
+        //Registrarse (crear usuario)
+        path: "/",
+        method: "POST",
+        authentication: true,
+        authorization: [ROLE.Admin],
+        middlewares: [],
         handler: (_, _1) => {throw new ApiError(HttpStatuses.NOT_IMPLEMENTED, "To do")}
-    }
+    },
+    {
+        //Traer usuario
+        path: "/:userId",
+        method: "GET",
+        authentication: true,
+        authorization: [ROLE.Admin, ROLE.Auditor],
+        middlewares: [],
+        handler: (_, _1) => {throw new ApiError(HttpStatuses.NOT_IMPLEMENTED, "To do")}
+    },
+    {
+        //Eliminar usuario
+        path: "/",
+        method: "DELETE",
+        authentication: true,
+        authorization: [ROLE.Admin],
+        middlewares: [],
+        handler: (_, _1) => {throw new ApiError(HttpStatuses.NOT_IMPLEMENTED, "To do")}
+    },
+    {
+        //Modificar usuario
+        path: "/:userId",
+        method: "PUT",
+        authentication: true,
+        authorization: [ROLE.Admin],
+        middlewares: [],
+        handler: (_, _1) => {throw new ApiError(HttpStatuses.NOT_IMPLEMENTED, "To do")}
+    },
 ]
 
 export default USERS_ROUTES
--- Backend/src/routes/users/usersController.ts
+++ Backend/src/routes/users/usersController.ts
@@ -1,7 +1,40 @@
 import { Request, Response } from "express";
 import prisma from "../../prisma";
+import ApiError from "../../utils/ApiError";
+import HttpStatuses from "../../utils/HttpStatus";
+import bcrypt from 'bcrypt'
+import json from 'jsonwebtoken'
+import { env } from "process";
 
 export async function getAllUsers(_ : Request, res : Response){
     const usuarios = await prisma.usuarios.findMany();
     return res.json(usuarios);
+}
+
+export async function login(req : Request, res : Response) {
+    const { nombre, password } = req.body;
+
+    if(!(nombre && password))
+        throw new ApiError(HttpStatuses.BAD_REQUEST, "Se necesita un nombre y una contraseña");
+
+    const usuario = await prisma.usuarios.findFirst({where: {nombre}});
+
+    if(!usuario)
+        throw new ApiError(HttpStatuses.BAD_REQUEST, "Usuario o contraseña incorrectos");
+
+    const correctPassword = await bcrypt.compare(password, usuario.contrasenia);
+    if(!correctPassword)
+        throw new ApiError(HttpStatuses.BAD_REQUEST, "Usuario o contraseña incorrectos");
+
+    const role = await prisma.roles.findUnique({where: {id: usuario.Roles_id}})
+
+    const token = json.sign({id: usuario.id, role}, env.JWT_PASSWORD);
+    res.cookie(env.AUTHENTICATION_COOKIE_NAME, token, {
+        expires: new Date(Date.now() + 8640000),
+        sameSite:'none',
+        secure:true,
+        httpOnly:false,
+        priority:"high",
+    });
+    res.status(HttpStatuses.OK.code).json(usuario);
 }
