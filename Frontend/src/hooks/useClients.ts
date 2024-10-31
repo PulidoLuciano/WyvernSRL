5a23f1cafb53b6cf681dde3068418f454bb38791
FelipeSarco17 <sarcofelipe456@gmail.com>
2024-10-31 20:51:56 -0300

    Filter function for clients table added

--- Frontend/src/hooks/useClients.ts
+++ Frontend/src/hooks/useClients.ts
@@ -9,24 +9,40 @@ export const useClients = () =>{
   const [error, setError] = useState<string | null>(null);
   const [deletes, setDeletes] =useState<Array<any> | null>(null)
 
-  const getAllClients = useCallback(async (platforms?:boolean,countries?:boolean) => {
+  const getAllClients = useCallback(async (platforms?:boolean,countries?:boolean,filterUrl?:string) => {
     setLoading(true);
     setError(null);
-    let url = "http://localhost:3000/clients/";
+
+    let filterUrlLast;
+    filterUrl? filterUrlLast = filterUrl.replace("?","&") : filterUrlLast = "";
+
+    let url = "http://localhost:3000/clients/";  
+    let includePlatformsCountries = "?include=id&include=nombre&include=correo&include=telefono&include=suscripto&include=Plataformas&include=Paises&include=borrado";
+    let includePlatforms = "?include=id&include=nombre&include=correo&include=telefono&include=suscripto&include=Plataformas&include=borrado";
+    let includeCountries = "?include=id&include=nombre&include=correo&include=telefono&include=suscripto&include=Paises&include=borrado";
     try {
   
         if (countries && platforms) {
-            url = url.concat("?include=id&include=nombre&include=correo&include=telefono&include=suscripto&include=Plataformas&include=Paises&borrado=false");
+          filterUrl? url = url.concat(includePlatformsCountries,filterUrlLast)  :
+            url = url.concat(includePlatformsCountries);
         }
         else if (platforms) {
-            url = url.concat("?include=id&include=nombre&include=correo&include=telefono&include=suscripto&include=Plataformas&borrado=false");
+          filterUrl? url = url.concat(includePlatforms,filterUrlLast)  :
+            url = url.concat(includePlatforms);
         } else if (countries){
-            url = url.concat("?include=id&include=nombre&include=correo&include=telefono&include=suscripto&include=Paises&borrado=false");
+          filterUrl? url = url.concat(includeCountries,filterUrlLast)  :
+            url = url.concat(includeCountries);
         }
-        const data = await clientsService.getAll(url); 
-
+        else
+        {
+          if(filterUrl) url = url.concat(filterUrl); 
+        }
         
-        setClients(data);
+        console.log(url);
+        
+        const data = await clientsService.getAll(url); 
+        
+        setClients(data)  
         } catch (err: any) {    
         setError(err.message);
         } finally {
