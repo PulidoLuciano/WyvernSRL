d75d9c2461b23116fc3355c39407e3d3ae26019f
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-11-01 22:55:55 -0300

    Test populate 3

--- Backend/src/utils/populate.ts
+++ Backend/src/utils/populate.ts
@@ -7,6 +7,7 @@ import PROVEEDORES_METODOS from "./data/PROVEEDORES_METODOS";
 import PROVEEDORES_MONEDAS from "./data/PROVEEDORES_MONEDAS";
 import CONTRATOS from "./data/CONTRATOS";
 import COMPRAS from "./data/COMPRAS";
+import { hashSync } from "bcrypt";
 
 const prisma = new PrismaClient()
 
@@ -40,11 +41,44 @@ async function populate(tabla : PopulateData) {
 }
 
 async function main(){
+    await prisma.$queryRaw`DROP TRIGGER [IF EXISTS] before_create_empleados;`
+    prisma.paises.create({data: {nombre: "Bhutan"}});
+    prisma.provincias.create({data: {nombre: "Juanlandia", Paises_id: 1}});
+    prisma.empleados.create({data:{ 
+        nombre: "Juan Manuel Lopez Asis", 
+        correo: "juancitoLopez@wyvern.com",
+        telefono: null,
+        dni: 431039123,
+        fechaContratacion: new Date(Date.now()).toISOString(),
+        sueldo: 4000,
+        Provincias_id: 1
+    }});
+    prisma.roles.create({data: {
+        nombre: "Admin"
+    }})
+    prisma.usuarios.create({ data: {
+        nombre: "JuanLopezAsis",
+        contrasenia: hashSync("1234",10),
+        Empleados_id: 1,
+        Roles_id: 1
+    }})
     await prisma.$queryRaw`SET @User = 1`
     for (let index = 0; index < ENTRY_POINTS.length; index++) {
         const element = ENTRY_POINTS[index];
         await populate(element);
     }
+    await prisma.$queryRaw`CREATE TRIGGER before_create_empleados
+    AFTER INSERT ON Empleados
+    FOR EACH ROW
+    BEGIN
+        -- Inserta en la tabla historial_empleado los datos del cliente que será modificado
+        INSERT INTO Empleados_historial(nombre, correo, telefono, dni, fechaContratacion, sueldo, Provincias_id, borrado)
+        VALUES (NEW.nombre, NEW.correo, NEW.telefono, NEW.dni, NEW.fechaContratacion, NEW.sueldo, NEW.Provincias_id, NEW.borrado);
+        
+        -- Inserta en la tabla auditoría el registro de la acción
+        INSERT INTO Auditoria_Empleados(fecha, tipo, Usuarios_id, Empleados_historial_id, Empleados_id)
+        VALUES (NOW(), 'Crear', @User, LAST_INSERT_ID(), NEW.id);
+    END;`
 }
 
 main().then(async () => {
