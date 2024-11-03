4499cc15064e33fa4a1968aae60f6cfbf134d0ed
JuanLopezAsis27 <juan.lopezasis27@hotmail.com>
2024-11-03 05:56:25 -0300

    Add employees table

---
+++ Frontend/src/service/employeesService.ts
@@ -0,0 +1,95 @@
+import { employeeType } from "../utils/types/employeeType";
+
+const getAllEmployees = async (url : string) => {
+    
+    const response = await fetch(`${url}`, {
+        mode: 'cors',
+        method: 'GET',
+        credentials: 'include',
+    })
+    
+    const data = await response.json();
+    
+    if (!response.ok) throw new Error(`${data.message}`);
+   
+    return data;
+
+}
+
+const createEmployee= async (url: string,obj: employeeType) => {
+    
+    const response = await fetch(`${url}/`, {
+        mode: 'cors',
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify({
+      
+        })
+    })
+
+    const data = await response.json();
+    
+    if (!response.ok) throw new Error(`${data.message}`);
+
+    return data;
+
+}
+
+const updateEmployee = async(id: number,obj:employeeType)=>{
+
+        const response = await fetch(`http://localhost:3000/providers/${id}`,{
+            mode:"cors",
+            method: "PUT",
+            credentials: "include",
+            headers:{
+                'Content-Type' : 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify({
+                
+            })
+        })
+        
+        const data = await response.json()
+        console.log(data);
+        if(!response.ok) throw new Error(`${data.message}`);
+        return data;
+ 
+}
+
+const deleteEmployee = async (url: string, ids: Array<any | null>) => {
+const response = await fetch(`${url}`,{
+            mode: 'cors',
+            method: 'DELETE',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+            },
+            body: JSON.stringify({ids})
+        })
+
+const data = await response.json();
+if (!response.ok) throw new Error(`${data.message}`);
+return data
+}
+
+const getOne = async (url : string) => {
+
+const response = await fetch(`${url}`, {
+    mode: 'cors',
+    method: 'GET',
+    credentials: 'include',
+})
+
+const data = await response.json();
+
+if (!response.ok) throw new Error(`${data.message}`);
+
+return data;
+
+}
+
+
+export const employeesService = {updateEmployee,getAllEmployees,getOne,createEmployee,deleteEmployee}
