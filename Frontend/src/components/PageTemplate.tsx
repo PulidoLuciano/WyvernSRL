c53abaf43c8912764829ee353664d65e2e18e903
FelipeSarco17 <sarcofelipe456@gmail.com>
2024-11-08 12:15:05 -0300

    Responsive bug fixed

--- Frontend/src/components/PageTemplate.tsx
+++ Frontend/src/components/PageTemplate.tsx
@@ -3,7 +3,7 @@ import Nav from './Nav'
 
 const PageTemplate = ({element}: {element:React.ReactElement}) => {
   return (
-    <div className='flex flex-col gap-y-10 laptop:flex gap-x-5'>
+    <div className='flex flex-col gap-y-10 laptop:flex laptop:flex-row gap-x-5'>
         <Nav/>
         {element}
     </div>
