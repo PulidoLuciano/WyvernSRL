6f8457b32770b9b556c3b03d179aa71ff7114738
FelipeSarco17 <sarcofelipe456@gmail.com>
2024-11-08 01:09:07 -0300

    Some titles errors fixed on suppliers detail, contract detail and sale detail

--- Frontend/src/pages/details/SupplierDetail.tsx
+++ Frontend/src/pages/details/SupplierDetail.tsx
@@ -337,7 +337,7 @@ const SupplierDetail = () => {
           <>
             <div className="grid grid-cols-2 my-6">
               <div className="col-span-2 flex gap-x-3 mb-4">
-                <h2 className="text-xl">Datos del cliente</h2>
+                <h2 className="text-xl">Datos del proveedor</h2>
                 <button onClick={handleClickEditable} className="bg-primary flex text-white rounded px-6 py-2">
                   <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
