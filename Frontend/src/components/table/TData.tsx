891c7955a28887ef118e98dde99b28eb4e85ad93
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-11-22 00:57:47 -0300

    Fix checkboxes in table on delete

--- Frontend/src/components/table/TData.tsx
+++ Frontend/src/components/table/TData.tsx
@@ -1,13 +1,13 @@
-
-import { useEffect } from 'react'
+import { useState } from 'react'
 import { TDataProps } from '../../utils/types/TableInterfaces'
 
 const TData = ({ id, selectedData,children,value, checkbox, onChange }: TDataProps) => {
 
-  let isChecked : boolean | undefined;
-  useEffect(()=>{
-    isChecked = selectedData?.includes(value!)
-  },[selectedData])
+  let isChecked
+
+  if(id){
+    isChecked = selectedData?.includes(String(id));
+  }
 
   if (checkbox) {
     return <td className=' p-2 '><input type="checkbox" checked={isChecked} id={id} value={value} onChange={onChange} className="me-2" />{children}</td>
